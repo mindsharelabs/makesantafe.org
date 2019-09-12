b5d512a1a606d65db5812602508a8c3a29b73efa
James Johnson <james@mindsharelabs.com>
2019-09-12 12:26:49 -0600

    added archive page.

--- category.php
+++ category.php
@@ -8,22 +8,23 @@ include 'layout/notice.php';
       <?php rewind_posts(); ?>
       <div class="row">
         <div class="col">
-          <h2 class="section-title"><?php _e('Posted in ', 'mindblank'); single_cat_title(); ?></h2>
+          <h2 class="mt-3 mb-2 section-title"><?php _e('Posted in ', 'mindblank'); single_cat_title(); ?></h2>
           <div class="row">
               <?php
 
               while (have_posts()) : the_post();
                 $image_url = aq_resize(get_the_post_thumbnail_url(), 300, 400, true, true);
-
-                echo '<div class="col-12 col-md-3">': 
+                echo '<div class="col-12 col-md-3">';
                   echo '<div class="card">';
-                    echo '<img src="' . $image_url . '" class="card-img-top loop-card" alt="' . the_title_attribute($attr) . '">';
-                    echo '<div class="card-body">':
-                      echo '<h5 class="card-title">' . get_the_title() . '</h5>':
-                      echo '<p class="card-text">' . get_the_excerpt() . '</p>':
-                      echo '<a href="' . get_permalink() . '" class="btn btn-primary">Read More</a>':
-                    echo '</div>':
-                  echo '</div>':
+                    if(has_post_thumbnail()) :
+                      echo '<img src="' . $image_url . '" class="card-img-top loop-card" alt="' . the_title_attribute($attr) . '">';
+                    endif;
+                    echo '<div class="card-body">';
+                      echo '<h5 class="card-title">' . get_the_title() . '</h5>';
+                      echo '<p class="card-text">' . get_the_excerpt() . '</p>';
+                      echo '<a href="' . get_permalink() . '" class="btn btn-primary">Read More</a>';
+                    echo '</div>';
+                  echo '</div>';
                 echo '</div>';
               endwhile; ?>
 
