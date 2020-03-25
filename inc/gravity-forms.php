5f31cd3633910823e0276c149c69d70b73cb4912
James W. Johnson <James@mindsharelabs.com>
2020-03-24 20:53:16 -0700

    site sync.

---
+++ inc/gravity-forms.php
@@ -0,0 +1,10 @@
+<?php
+
+add_action( 'gform_after_submission_27', 'make_map_user_to_entry', 10, 2 );
+function make_map_user_to_entry( $entry, $form ) {
+    $user_id = $entry['created_by'];
+    // set the arguments for the add_user_meta() function
+    $meta_key = 'liability_entry';
+    $meta_value = array( 'entry_id' => $entry['id'], 'form_id' => $form['id'] );
+    $return = add_user_meta( $user_id, $meta_key, $meta_value);
+}
---
+++ template-skinny.php
@@ -0,0 +1,46 @@
+<?php
+//Template Name: Skinny Column
+if(is_account_page()) :
+  acf_form_head();
+endif;
+get_header();
+include 'layout/page-header.php';
+include 'layout/notice.php';
+?>
+    <main role="main" aria-label="Content" class="container">
+        <div class="row">
+            <div class="col-12 col-md-8 offset-md-2">
+                <!-- section -->
+                <section>
+                    <?php
+                    if(!get_field('show_header')):
+                      echo '<h1>';
+                        the_title();
+                      echo '</h1>';
+                    endif;
+                    if (have_posts()): while (have_posts()) : the_post(); ?>
+                        <!-- article -->
+                        <article id="post-<?php the_ID(); ?>" <?php post_class('mt-3'); ?>>
+
+                            <?php the_content(); ?>
+
+                            <?php comments_template('', true); // Remove if you don't want comments ?>
+
+                            <br class="clear">
+
+                            <?php mapi_post_edit(); ?>
+
+                        </article>
+                        <!-- /article -->
+                    <?php endwhile; ?>
+                    <?php endif; ?>
+
+                </section>
+            </div>
+
+        </div>
+        <!-- /section -->
+    </main>
+
+<?php include 'layout/top-footer.php';
+get_footer();
