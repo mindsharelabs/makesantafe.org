e406cf52414e862d450b89f805b59bb2fe87b775
James W. Johnson <me@thejameswilliam.com>
2022-12-22 19:38:00 -0700

    upgrade tool pages.

--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ if (function_exists('add_theme_support')) {
     add_image_size('small-square', 400, 400, array('center', 'center'));
     add_image_size('page-header', 1500, 300, array('center', 'center'));
     add_image_size('horz-thumbnail', 300, 150, array('center', 'center'));
+    add_image_size('horz-thumbnail-lg', 500, 300, array('center', 'center'));
 
 
 
