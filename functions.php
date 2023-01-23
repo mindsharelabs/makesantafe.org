72265de9865f774584277f4b99ea0dcb8d1e5a86
James W Johnson <me@thejameswilliam.com>
2023-01-23 14:28:57 -0700

    removed speacial tribe calendar fixes.

--- functions.php
+++ functions.php
@@ -20,21 +20,6 @@ include_once 'inc/gravity-forms.php';
 \*------------------------------------*/
 
 
-/*
- * Possible solution for Single Event page 404 errors where the WP_Query has an attachment set
- * IMPORTANT: Flush permalinks after pasting this code: http://tri.be/support/documentation/troubleshooting-404-errors/
- * Updated to work with post 3.10 versions
- */
-function tribe_attachment_404_fix () {
-	if (class_exists('Tribe__Events__Main')) {
-		remove_action( 'init', array( Tribe__Events__Main::instance(), 'init' ), 10 );
-		add_action( 'init', array( Tribe__Events__Main::instance(), 'init' ), 1 );
-	}
-}
-
-add_action( 'after_setup_theme', 'tribe_attachment_404_fix' );
-
-
 
 if (!isset($content_width)) {
     $content_width = 900;
