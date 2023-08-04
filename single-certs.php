9f02cd6d154388acb3e809d3520c7c0216fccb41
James W. Johnson <me@thejameswilliam.com>
2023-08-04 11:07:28 -0600

    removed tool access on badge pages.

--- single-certs.php
+++ single-certs.php
@@ -2,7 +2,6 @@
 include 'layout/page-header.php';
 include 'layout/notice.php';
 
-$allowed_shop_space = get_field('allowed_shop_space');
 $p_makers = make_get_badged_members(get_the_id());
 
 ?>
@@ -31,18 +30,6 @@ $p_makers = make_get_badged_members(get_the_id());
                     the_content();
                   echo '</div>';
 
-                  if($allowed_shop_space) :
-                    echo '<div class="col-12 mt-5 pt-3">';
-                      echo '<div class="row justify-content-center">';
-                        echo '<div class="col-12 text-center">';
-                          echo '<h2>With this badge you gain access to: </h3>';
-                        echo '</div>';
-                        foreach($allowed_shop_space as $term) :
-                          echo make_output_shop_space($term);
-                        endforeach;
-                      echo '</div>';
-                    echo '</div>';
-                  endif;
                 endwhile;
               endif;
               ?>
