e604f5768bfa41057b359c785f0fc47b1d9ca85a
James W Johnson <me@thejameswilliam.com>
2024-01-28 15:32:50 -0700

    refactored author page for better markup.

--- author.php
+++ author.php
@@ -4,10 +4,26 @@
   $author = get_user_by( 'slug', get_query_var( 'author_name' ) );
   $maker_id = $author->ID;
   $public = get_field('display_profile_publicly', 'user_' . $maker_id);
+
+
+  $args = array(
+    'author'        =>  $author->ID,
+    'orderby'       =>  'post_date',
+    'order'         =>  'ASC',
+    'posts_per_page' => -11
+  );
+  $maker_posts = get_posts( $args );
+
+
  ?>
 <main role="main" aria-label="Content" <?php post_class('container'); ?>>
   <section class="maker-profile">
-    <?php if($public) :
+    
+    
+    
+    
+    <?php
+    if($public) :
       $user_obj = get_userdata( $maker_id );
       $current_user_id = get_current_user_id();
       $photo = get_field('photo', 'user_' . $maker_id);
@@ -23,23 +39,27 @@
       $title = get_field('title', 'user_' . $maker_id );
       
       ?>
-    <div class="row">
-      <div class="col-12">
-        <?php
-        echo '<header class="fancy-header d-flex mt-5">';
-          echo '<div class="header-flex-item">';
-            echo '<h1 class="page-title ">';
-              echo $name;
-            echo '</h1>';
-          echo '</div>';
-          echo '<div class="header-flex-svg">';
-            include get_template_directory() . '/inc/svgheader.svg';
-          echo '</div>';
-        echo '</header>';
-        echo '<hr class="clear">';
-        ?>
+      <div class="row">
+        <div class="col-12">
+          <?php
+          echo '<header class="fancy-header d-flex mt-5">';
+            echo '<div class="header-flex-item">';
+              echo '<h1 class="page-title ">';
+                echo $name;
+              echo '</h1>';
+            echo '</div>';
+            echo '<div class="header-flex-svg">';
+              include get_template_directory() . '/inc/svgheader.svg';
+            echo '</div>';
+          echo '</header>';
+          echo '<hr class="clear">';
+          ?>
+        </div>
       </div>
-    </div>
+
+
+
+
     <div class="row">
       <div class="col-12 col-md-4">
         <?php
@@ -52,9 +72,7 @@
 
         if($current_user_id == $maker_id) :
           echo '<a href="/my-account/make-profile/" class="btn btn-default btn-block mt-4">Edit My Profile</a>';
-
         endif;
-
 
         $maker_badges = get_field('certifications', 'user_' . $maker_id );
         $all_badges = get_posts(array(
@@ -69,7 +87,7 @@
 
         if($badges):  
           echo '<h2 class="sidebar-title mt-4">Badges</h4>';
-          echo '<div class="d-flex maker-badges flex-wrap">';
+          echo '<div class="d-flex maker-badges flex-wrap justify-content-between">';
             foreach ($badges as $key => $badge) :
               if(is_array($maker_badges)) :
 
@@ -82,7 +100,7 @@
 
                 
               if($image = get_field('badge_image', $badge)) :
-                echo '<div class="badge-image-holder ' . $class . ' m-1">';
+                echo '<div class="badge-image-holder mb-3 ' . $class . ' m-1">';
                   echo '<a href="' . get_permalink($badge) . '" title="' . get_the_title($badge) . '">';
                     echo wp_get_attachment_image($image);
                     echo '<h3 class="text-center badge-title">' . get_the_title($badge) . '</h3>';
@@ -100,42 +118,36 @@
 
         ?>
       </div>
-      <div class="col-12 col-md-8">
-        <?php
-        if($bio) :
+
+
+
+      
+      <?php
+      echo '<div class="col-12 col-md-8 mb-3">';
+        if($bio) : 
           echo $bio;
-        endif;
-        ?>
-        <hr>
-        <?php
+        endif; 
         if($gallery) :
           echo '<div class="row gy-3 mb-4">';
-          foreach ($gallery as $key => $image) :
-
-            $image_elem = wp_get_attachment_image( $image['image']['ID'], 'small-square', false, array('class' => 'card-img-top'));
-            if($image_elem):
-              echo '<div class="col-12 col-md-4">';
-                echo '<div class="card">';
-                  echo $image_elem;
-                  if($image['caption']) :
-                    echo '<div class="card-body small text-center p-1">' . $image['caption'] . '</div>';
-                  endif;
+            foreach ($gallery as $key => $image) :
+              $image_elem = wp_get_attachment_image( $image['image']['ID'], 'small-square', false, array('class' => 'card-img-top'));
+              if($image_elem):
+                echo '<div class="col-12 col-md-4">';
+                  echo '<div class="card">';
+                    echo $image_elem;
+                    if($image['caption']) :
+                      echo '<div class="card-body small text-center p-1">' . $image['caption'] . '</div>';
+                    endif;
+                  echo '</div>';
                 echo '</div>';
-              echo '</div>';
-            endif;
-          endforeach;
-          echo '</div>';
+              endif;
+            endforeach;
+            echo '</div>';
         endif;
-        ?>
-      </div>
-    </div>
+      echo '</div>';
+      ?>
+    
   </section>
-  <?php else : ?>
-    <div class="row">
-      <div class="col-12 mt-5 pt-5 mb-5 pb-5">
-        <h3 class="text-center">This user's profile is private</h3>
-      </div>
-    </div>
 
 
   <?php endif; ?>
