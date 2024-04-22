f0d4283798f07d9be7c6311dfb940f04cbcdd0a6
James W Johnson <me@thejameswilliam.com>
2024-04-21 21:38:57 -0600

    added a bit of space before cart.

--- layout/top-header.php
+++ layout/top-header.php
@@ -20,7 +20,7 @@
               if(class_exists('woocommerce')) :
                 $cart_count = WC()->cart->cart_contents_count; // Set variable for cart item count
                 $cart_url = wc_get_cart_url(); 
-                echo '<a href="' . $cart_url . '" class="icon cart-icon" style="vertical-align: top;" title="Shopping Cart">';
+                echo '<a href="' . $cart_url . '" class="ms-4 icon cart-icon" style="vertical-align: top;" title="Shopping Cart">';
                   echo '<i class="fas fa-shopping-cart fa-sm"></i>';
                   if($cart_count > 0) :
                     echo '<span class="cart-contents-count"><i class="text-success fa-xs fa fa-inverse fa-circle-' . $cart_count . '"></i></span>';
