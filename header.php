5cf08ac0c548b7368b293d3e3f9b1486e6d101a9
James W Johnson <me@thejameswilliam.com>
2024-04-23 23:17:10 -0600

    added google tracking tag.

--- header.php
+++ header.php
@@ -46,6 +46,15 @@
     /></noscript>
     <!-- End Meta Pixel Code -->
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11012918498"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-11012918498');
+    </script>
     
 </head>
 
