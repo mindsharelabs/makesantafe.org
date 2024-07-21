f4533b0d72bdfc1cf1d94f307fa76a8c373ff3a2
James W. Johnson <me@thejameswilliam.com>
2024-07-21 15:57:31 -0600

    version bump.

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * URL: https://mind.sh/are | @mindblank
  *
  */
-define('THEME_VERSION', '3.6.0');
+define('THEME_VERSION', '3.7.1');
 /*------------------------------------*\
     External Modules/Files
 \*------------------------------------*/
