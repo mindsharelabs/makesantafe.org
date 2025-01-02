c54ac8ab1f20b010e896d1b36ef5f3320790fe83
James W Johnson <me@thejameswilliam.com>
2025-01-02 09:46:34 -0700

    better class information on checkout.

--- tribe/tickets/components/attendees-list/attendees.php
+++ tribe/tickets/components/attendees-list/attendees.php
@@ -1,3 +1,12 @@
 <?php
-
+echo '<div class="class-information">';
+    echo '<h2>Class Information</h3>';
+    echo '<div class="row">';
+        echo '<div class="col-12">';
+            $woocommerce_account_page_url = get_permalink( get_option('woocommerce_myaccount_page_id') );
+            mapi_write_log($woocommerce_account_page_url);
+            echo '<p>You can view your class information on your <a href="' . $woocommerce_account_page_url . '">account page</a>. If you have any questions, please contact us at <a href="mailto:build@makesantafe.org">build@makesantafe.org</a></p>';
+        echo '</div>';
+    echo '</div>';
+echo '</div>';
 //Intentionally left blank
