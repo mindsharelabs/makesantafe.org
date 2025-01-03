86a36483d6cc437071a6e7a1fc5318b30b5a9590
James W Johnson <me@thejameswilliam.com>
2025-01-03 09:13:35 -0700

    vesion bump to clear caching.

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * URL: https://mind.sh/are | @mindblank
  *
  */
-define('THEME_VERSION', '3.8.1');
+define('THEME_VERSION', '3.9.0');
 /*------------------------------------*\
     External Modules/Files
 \*------------------------------------*/
