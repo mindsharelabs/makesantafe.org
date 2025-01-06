a6cf0793bcda9608191cc482e9944fb7f44a962c
James W Johnson <me@thejameswilliam.com>
2025-01-05 22:39:51 -0700

    removed logging.

--- tribe/tickets/components/attendees-list/attendees.php
+++ tribe/tickets/components/attendees-list/attendees.php
@@ -4,7 +4,6 @@ echo '<div class="class-information">';
     echo '<div class="row">';
         echo '<div class="col-12">';
             $woocommerce_account_page_url = get_permalink( get_option('woocommerce_myaccount_page_id') );
-            mapi_write_log($woocommerce_account_page_url);
             echo '<p>You can view your class information on your <a href="' . $woocommerce_account_page_url . '">account page</a>. If you have any questions, please contact us at <a href="mailto:build@makesantafe.org">build@makesantafe.org</a></p>';
         echo '</div>';
     echo '</div>';
