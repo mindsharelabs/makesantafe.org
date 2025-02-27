ff2b2b0ffcf60e822861997d2ebcce8766d194ae
James W Johnson <me@thejameswilliam.com>
2025-02-26 20:32:53 -0700

    moved related events to inside container for better margins

--- inc/mindshare-events.php
+++ inc/mindshare-events.php
@@ -2,7 +2,7 @@
 
 
 
-add_action('mindevents_after_single_container', function() {
+add_action('mindevents_single_after_events', function() {
     // mapi_write_log();
     $related_events = new WP_Query(
       array(
@@ -32,39 +32,41 @@ add_action('mindevents_after_single_container', function() {
         ),
       ));
     if ( $related_events->have_posts() ) :
-      echo '<div class="related-events row my-3">';
-        echo '<h2>' . __('Related Events', 'mind-events') . '</h2>';
-        while ( $related_events->have_posts() ) {
-          $related_events->the_post();
-          //output the event card
-          echo '<div class="col-12 col-md-3">';
-            echo '<div class="event-card card  d-flex justify-content-center h-100">';
-                
-                echo '<div class="event-card-image card-img-top">';
-                    echo '<a href="' . get_the_permalink() . '">';
-                        the_post_thumbnail('horz-thumbnail-lg', array('class' => 'img-fluid card-img-top'));
-                    echo '</a>';
-                echo '</div>';
-
-                echo '<div class="event-card-content card-body">';
-                    echo '<a href="' . get_the_permalink() . '">';
-                        echo '<h3 class="h4">' . get_the_title() . '</h3>';
-                    echo '</a>';
-                    echo '<div class="event-datespan">';
-                        $first_event = new DateTime(get_post_meta(get_the_ID(), 'first_event_date', true));
-                        $startdate = $first_event->format('F j');
-                        $last_event = new DateTime(get_post_meta(get_the_ID(), 'last_event_date', true));
-                        $enddate = $last_event->format('F j');
-                        echo 'Classes from <strong>' . $startdate . '</strong> to <strong>' . $enddate . '</strong>';
+      
+        echo '<div class="related-events row my-3">';
+            echo '<h2>' . __('Related Events', 'mind-events') . '</h2>';
+            while ( $related_events->have_posts() ) :
+            $related_events->the_post();
+            //output the event card
+            echo '<div class="col-12 col-md-3">';
+                echo '<div class="event-card card  d-flex justify-content-center h-100">';
+                    
+                    echo '<div class="event-card-image card-img-top">';
+                        echo '<a href="' . get_the_permalink() . '">';
+                            the_post_thumbnail('horz-thumbnail-lg', array('class' => 'img-fluid card-img-top'));
+                        echo '</a>';
                     echo '</div>';
-                    echo '<p class="post-excerpt">' . get_the_excerpt() . '</p>';
+
+                    echo '<div class="event-card-content card-body">';
+                        echo '<a href="' . get_the_permalink() . '">';
+                            echo '<h3 class="h4">' . get_the_title() . '</h3>';
+                        echo '</a>';
+                        echo '<div class="event-datespan">';
+                            $first_event = new DateTime(get_post_meta(get_the_ID(), 'first_event_date', true));
+                            $startdate = $first_event->format('F j');
+                            $last_event = new DateTime(get_post_meta(get_the_ID(), 'last_event_date', true));
+                            $enddate = $last_event->format('F j');
+                            echo 'Classes from <strong>' . $startdate . '</strong> to <strong>' . $enddate . '</strong>';
+                        echo '</div>';
+                        echo '<p class="post-excerpt">' . get_the_excerpt() . '</p>';
+                    echo '</div>';
                 echo '</div>';
             echo '</div>';
-          echo '</div>';
-          
-        }
-  
-      echo '</div>';
+            
+            endwhile;
+    
+        echo '</div>';
+      
     endif;
     wp_reset_postdata();
   }, 99);
