848361dad6dd90689f6ae7c93ba38cd511e1dceb
James W. Johnson <me@thejameswilliam.com>
2025-04-27 11:47:22 -0600

    bold adjustments.

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -9,9 +9,26 @@ function make_map_user_to_entry( $entry, $form ) {
     $return = add_user_meta( $user_id, $meta_key, $meta_value);
 }
 
-
-add_filter( 'gform_allowable_tags', 'allow_br_tag' );
-function allow_br_tag( $allowed_tags ) {
+mapi_write_log( 'make_allowable_tags' );
+add_filter( 'gform_allowable_tags', 'make_allowable_tags' );
+function make_allowable_tags( $allowed_tags ) {
+    mapi_write_log( 'make_allowable_tags' );
+    mapi_write_log( $allowed_tags );
     $allowed_tags['br'] = true;
     return $allowed_tags;
+}
+
+
+
+add_filter( 'gform_field_content', 'make_allow_html_in_consent_description', 10, 5 );
+function make_allow_html_in_consent_description( $field_content, $field, $value, $lead_id, $form_id ) {
+    // Check if the field is a consent field
+    if ( $field->type === 'consent' ) {
+        // Allow HTML tags in the field description
+        $field->description = wp_kses_post( $field->description );
+    }
+    mapi_write_log( 'make_allow_html_in_consent_description' );
+    mapi_write_log( $field->description );
+    mapi_write_log( $field_content );
+    return $field_content;
 }
