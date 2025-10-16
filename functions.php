fb0c784ccc67785d6d01699d4d3a6a5372f4dc61
James W Johnson <me@thejameswilliam.com>
2025-10-15 18:51:30 -0600

    better tabbed navigation for my-account page.

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * URL: https://mind.sh/are | @mindblank
  *
  */
-define('THEME_VERSION', '4.4.1');
+define('THEME_VERSION', '4.5.1');
 /*------------------------------------*\
     External Modules/Files
 \*------------------------------------*/
