00e251dae41a34b6806d5fc0fe8aa16fe85fdf81
James W. Johnson <me@thejameswilliam.com>
2026-05-29 18:46:29 -0600

    addded author and pposted date to blog category page.

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * URL: https://mind.sh/are | @mindblank
  *
  */
-define('THEME_VERSION', '4.5.3');
+define('THEME_VERSION', '4.6.3');
 /*------------------------------------*\
     External Modules/Files
 \*------------------------------------*/
