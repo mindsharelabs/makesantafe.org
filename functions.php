e12654f1b280bd6f96e9d6b65260e062579d63d5
James W. Johnson <me@thejameswilliam.com>
2026-07-02 09:18:59 -0600

    new font awesome icons.

--- functions.php
+++ functions.php
@@ -281,7 +281,8 @@ function mindblank_header_scripts()
 
         
 
-        wp_register_script('fontawesome', 'https://kit.fontawesome.com/936fcf5834.js', array(), THEME_VERSION, true);
+
+        wp_register_script('fontawesome', 'https://kit.fontawesome.com/5bcc5329ee.js', array(), THEME_VERSION, true);
         wp_enqueue_script('fontawesome');
         add_action('wp_head', function() {
           echo '<link rel="preconnect" href="https://kit-pro.fontawesome.com">';
